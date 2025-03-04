e6ac77e8236b52c95860dda74c67889953da0f8e
MayMetwalli <maymetwalli.27@gmail.com>
2025-03-05 00:56:40 +0200

    ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,16 +3,18 @@ import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpLoaderFactory } from './core/utilis/httpLoadFiles';
+import { headersInterceptor } from './core/interceptors/headers.interceptor';
 
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([headersInterceptor])), 
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
